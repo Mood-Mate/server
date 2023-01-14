bc54d3a0b3f6be82dec73f8a86ef767bbe03c028
hyungchulkim <nightletter65@gmail.com>
2023-01-15 08:35:02 +0900

    [+] 구글 or 카카오 로그인

---
+++ src/main/java/com/pado/socialdiary/api/member/entity/LoginProvider.java
@@ -0,0 +1,7 @@
+package com.pado.socialdiary.api.member.entity;
+
+public enum LoginProvider {
+    GOOGLE,
+    KAKAO,
+    NAVER;
+}
---
+++ src/main/java/com/pado/socialdiary/api/member/service/OAuth2SuccessHandler.java
@@ -0,0 +1,35 @@
+package com.pado.socialdiary.api.member.service;
+
+import com.pado.socialdiary.api.common.config.security.JwtProvider;
+import com.pado.socialdiary.api.common.dto.response.TokenResponse;
+import com.pado.socialdiary.api.member.entity.Member;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2User principal = (OAuth2User) authentication.getPrincipal();
+        TokenResponse tokenResponse = jwtProvider.generateToken(principal);
+
+        log.info("OAuth2SuccessHandler={}",tokenResponse.getAccessToken());
+        response.setHeader("Authorization", tokenResponse.getAccessToken());
+    }
+}
