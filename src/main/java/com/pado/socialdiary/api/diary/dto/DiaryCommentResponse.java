b866c1e97990863cda7ccda590bf50889ccae97b
hyungchulkim <nightletter65@gmail.com>
2023-02-06 11:26:52 +0900

    [+] 다이어리 댓글 생성 리턴 값 수정

--- src/main/java/com/pado/socialdiary/api/diary/dto/DiaryCommentResponse.java
+++ src/main/java/com/pado/socialdiary/api/diary/dto/DiaryCommentResponse.java
@@ -1,14 +1,13 @@
 package com.pado.socialdiary.api.diary.dto;
 
 import com.pado.socialdiary.api.common.constants.YesNoCode;
-import com.pado.socialdiary.api.diary.entity.DiaryComment;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class DiaryCommentResponse {
 
     private Integer diaryCommentId;
@@ -17,13 +16,4 @@ public class DiaryCommentResponse {
     private String contents;
     private YesNoCode delAt;
     private LocalDateTime regDt;
-
-    public DiaryCommentResponse(DiaryComment diaryComment) {
-        this.diaryCommentId = diaryComment.getDiaryCommentId();
-        this.diaryId = diaryComment.getDiaryId();
-        this.memberId = diaryComment.getMemberId();
-        this.contents = diaryComment.getContents();
-        this.delAt = diaryComment.getDelAt();
-        this.regDt = diaryComment.getRegDt();
-    }
 }
