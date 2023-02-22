d80eefb0be4ef4a3ab2d1e97715354cfd801faa2
hyungchulkim <nightletter65@gmail.com>
2023-02-22 18:44:45 +0900

    [fix] ExceptionHandlers.java

--- src/main/java/com/pado/socialdiary/api/exception/ExceptionHandlers.java
+++ src/main/java/com/pado/socialdiary/api/exception/ExceptionHandlers.java
@@ -2,20 +2,25 @@ package com.pado.socialdiary.api.exception;
 
 import com.pado.socialdiary.api.common.dto.ExceptionDto;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
-@RestControllerAdvice
+@Slf4j
+@RestControllerAdvice(annotations = RestController.class)
 public class ExceptionHandlers {
 
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity runtimeExceptionHandler(HttpServletRequest request, RuntimeException runtimeException) {
-        ExceptionDto exceptionDto = new ExceptionDto(runtimeException.getMessage(), request.getRequestURI());
+    public ResponseEntity runtimeExceptionHandler(HttpServletRequest request, RuntimeException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.toString(), request.getRequestURI());
+
+        log.error("[ exception handling ] ={}", e);
 
         return ResponseEntity
                 .internalServerError()
