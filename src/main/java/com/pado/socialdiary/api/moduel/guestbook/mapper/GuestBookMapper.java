5295c171a32d3afd4078d2f3969ac2c0771451ea
myandue <97776790+myandue@users.noreply.github.com>
2023-02-27 02:09:03 +0900

    [fix] mapper @Param 어노테이션 추가

--- src/main/java/com/pado/socialdiary/api/moduel/guestbook/mapper/GuestBookMapper.java
+++ src/main/java/com/pado/socialdiary/api/moduel/guestbook/mapper/GuestBookMapper.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.pado.socialdiary.api.moduel.guestbook.dto.GuestBookResponse;
 import com.pado.socialdiary.api.moduel.guestbook.entity.GuestBook;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface GuestBookMapper {
 
     void saveGuestBook(GuestBook guestBook);
-    List<GuestBookResponse> findGuestBook(Integer guestBookId, Integer memberId);
+    List<GuestBookResponse> findGuestBook(@Param("guestBookId") Integer guestBookId, @Param("memberId") Integer memberId);
     void deleteGuestBook(Integer guestBookId);
 }
