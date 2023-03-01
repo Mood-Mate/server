28334af37832f7c349286ed09af11324d18ea34c
myandue <97776790+myandue@users.noreply.github.com>
2023-03-02 00:00:07 +0900

    [+] CI/CD - 배포 스크립트 생성

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+CURRENT_PID=$(pgrep -f socialdiary.api)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> socialdiary.api 배포"
+cd /var/lib/jenkins/workspace/ci-cd-process/build/libs
+nohup java -jar -Dspring.profiles.active=dev socialdiary.api-0.0.1-SNAPSHOT.jar &
