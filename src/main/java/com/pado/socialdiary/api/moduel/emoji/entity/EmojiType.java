57bc95e7677a63863f4723980b245ccedbf0b7e4
khc <nightletter65@gmail.com>
2023-03-11 05:42:43 +0900

    [fix] 이모지 종류 굳히기

--- src/main/java/com/pado/socialdiary/api/moduel/emoji/entity/EmojiType.java
+++ src/main/java/com/pado/socialdiary/api/moduel/emoji/entity/EmojiType.java
@@ -1,5 +1,8 @@
 package com.pado.socialdiary.api.moduel.emoji.entity;
 
 public enum EmojiType {
-    좋아요, 슬퍼요, 화나요, 그저그래요;
+    LOVE,
+    SAD,
+    ANGRY,
+    STUPID;
 }
--- src/test/java/com/pado/socialdiary/api/emoji/service/EmojiServiceTest.java
+++ src/test/java/com/pado/socialdiary/api/emoji/service/EmojiServiceTest.java
@@ -1,48 +1,110 @@
-//package com.pado.socialdiary.api.emoji.service;
-//
-//import com.pado.socialdiary.api.moduel.emoji.dto.SympathyRequest;
-//import com.pado.socialdiary.api.moduel.emoji.entity.EmojiType;
-//import com.pado.socialdiary.api.moduel.emoji.mapper.EmojiMapper;
-//import com.pado.socialdiary.api.moduel.member.repository.MemberMapper;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.Mockito.*;
-//
-//@SpringBootTest
-//class EmojiServiceTest {
-//
-//    @Autowired
-//    EmojiMapper emojiMapper;
-//
-//    @MockBean
-//    MemberMapper memberMapper;
-//
-//    @Test
-//    @Transactional
-//    void createSympathy() {
-//        Integer tmpDiaryId = 2;
-//
-//        Member member = mock(Member.class);
-//        when(member.getMemberId()).thenReturn(1);
-//
-//        SympathyRequest request = new SympathyRequest();
-//        request.setMemberId(member.getMemberId());
-//        request.setDiaryId(tmpDiaryId);
-//        request.setEmojiType(EmojiType.좋아요);
-//
-//        emojiMapper.createSympathy(request);
-//        Optional<Integer> existSympathy = emojiMapper.existSympathy(request.getMemberId(), request.getDiaryId());
-//
-//        if(existSympathy.isEmpty()) {
-//            emojiMapper.createSympathy(request);
-//        } else {
-//            emojiMapper.deleteSympathy(request);
-//        }
-//    }
-//}
+package com.pado.socialdiary.api.emoji.service;
+
+import com.pado.socialdiary.api.moduel.diary.entity.Diary;
+import com.pado.socialdiary.api.moduel.diary.service.DiaryService;
+import com.pado.socialdiary.api.moduel.emoji.dto.SympathyRequest;
+import com.pado.socialdiary.api.moduel.emoji.entity.EmojiType;
+import com.pado.socialdiary.api.moduel.emoji.mapper.EmojiMapper;
+import com.pado.socialdiary.api.moduel.emoji.service.EmojiService;
+import com.pado.socialdiary.api.moduel.member.dto.MemberJoinRequest;
+import com.pado.socialdiary.api.moduel.member.entity.Member;
+import com.pado.socialdiary.api.moduel.member.repository.MemberRepository;
+import com.pado.socialdiary.api.moduel.member.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import static com.pado.socialdiary.api.moduel.member.entity.GenderType.ETC;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmojiServiceTest {
+
+    @Autowired
+    EmojiService emojiService;
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    DiaryService diaryService;
+
+    @Autowired
+    EmojiMapper emojiMapper;
+
+    private final String BEFORE_MEMBER_EMAIL = "before@test.com";
+
+    @BeforeEach
+    void before() throws IOException {
+        MemberJoinRequest request = new MemberJoinRequest();
+        request.setEmail(BEFORE_MEMBER_EMAIL);
+        request.setPassword("1234");
+        request.setName("testName");
+        request.setNickname("testNickname");
+        request.setGender(ETC);
+        request.setYear(2000);
+        request.setMonth(1);
+        request.setDayOfMonth(1);
+
+        memberService.join(request);
+    }
+
+    @Test
+    @Transactional
+    void createSympathyTest() {
+
+//        given
+        Member findMember = memberRepository.findByEmail(BEFORE_MEMBER_EMAIL).get();
+
+        Diary mockDiary = Mockito.mock(Diary.class);
+        Mockito.when(mockDiary.getDiaryId()).thenReturn(111);
+
+        SympathyRequest sympathyRequest = new SympathyRequest();
+        sympathyRequest.setMemberId(findMember.getMemberId());
+        sympathyRequest.setDiaryId(mockDiary.getDiaryId());
+        sympathyRequest.setEmojiType(EmojiType.LOVE);
+
+//        when
+        emojiService.createOrDeleteSympathy(findMember, sympathyRequest);
+        Integer existSympathy = emojiMapper.existSympathy(findMember.getMemberId(), mockDiary.getDiaryId()).get();
+
+//        then
+        assertNotNull(existSympathy);
+    }
+
+    @Test
+    @Transactional
+    void deleteSympathyTest() {
+
+//        given
+        Member findMember = memberRepository.findByEmail(BEFORE_MEMBER_EMAIL).get();
+        Diary mockDiary = Mockito.mock(Diary.class);
+        Mockito.when(mockDiary.getDiaryId()).thenReturn(111);
+
+        SympathyRequest sympathyRequest = new SympathyRequest();
+        sympathyRequest.setMemberId(findMember.getMemberId());
+        sympathyRequest.setDiaryId(mockDiary.getDiaryId());
+        sympathyRequest.setEmojiType(EmojiType.LOVE);
+
+//        when
+//        공감 클릭 1번 -> 공감 생성
+        emojiService.createOrDeleteSympathy(findMember, sympathyRequest);
+        Integer createdSympathy = emojiMapper.existSympathy(findMember.getMemberId(), mockDiary.getDiaryId()).get();
+
+//        공감 클릭 1번 더 -> 공감 삭제
+        emojiService.createOrDeleteSympathy(findMember, sympathyRequest);
+
+//        then -> Not null Check, Optional<T>.get() exception Check
+        assertNotNull(createdSympathy);
+        assertThrows(NoSuchElementException.class, () -> emojiMapper.existSympathy(findMember.getMemberId(), mockDiary.getDiaryId()).get());
+    }
+}
