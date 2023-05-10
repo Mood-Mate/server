c43ea16c222527c1f44497b693fd59569988c0d1
khc <nightletter65@gmail.com>
2023-05-10 18:35:05 +0900

    [fix] oauth 성공 핸들러

--- src/main/java/com/pado/socialdiary/api/config/security/oauth/OAuthSuccessHandler.java
+++ src/main/java/com/pado/socialdiary/api/config/security/oauth/OAuthSuccessHandler.java
@@ -9,19 +9,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
+public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Value("${front.url}")
+    private String frontUrl;
 
     private final JwtProvider jwtProvider;
 
@@ -30,11 +37,16 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
         OAuth2User principal = (OAuth2User) authentication.getPrincipal();
         AuthorizationDto.TokenResponse tokenResponse = jwtProvider.generateToken(principal);
 
-        ServletOutputStream outputStream = response.getOutputStream();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(outputStream, tokenResponse.getAccessToken());
-        outputStream.flush();
+        response.sendRedirect(
+                UriComponentsBuilder.fromUriString(frontUrl + request.getRequestURI())
+                        .queryParam("access_token", tokenResponse.getAccessToken())
+                        .build()
+                        .encode(StandardCharsets.UTF_8)
+                        .toUriString()
+        );
 
         log.info("OAuth2SuccessHandler={}",tokenResponse.getAccessToken());
     }
+
+
 }
